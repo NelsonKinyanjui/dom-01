a3e8b8a447aafeaea354845e9c8c3f7575d3d06a
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-05 09:09:58 +0300

    wrap up dom exercise

--- assets/dom-01.js
+++ assets/dom-01.js
@@ -19,4 +19,24 @@ heading.style.color = 'blue';
 heading.textContent = 'I am blue h3';
 container.appendChild(heading);
 
+const body = document.createElement('div');
 
+body.classList.add('body');
+
+const headerOne = document.createElement('h1');
+
+headerOne.classList.add('headerOne');
+
+headerOne.textContent = "I'm in a Div";
+
+body.appendChild(headerOne);
+
+const text = document.createElement('P');
+
+text.classList.add('text');
+
+text.textContent = "ME TOO";
+
+body.appendChild(text);
+
+container.appendChild(body);
