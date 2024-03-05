ae9107b29862e36c5d6586195630ff6fcfcc1496
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-05 09:03:32 +0300

    solve problem one

--- assets/dom-01.js
+++ assets/dom-01.js
@@ -9,3 +9,14 @@ paragraph.style.color = 'red';
 paragraph.textContent = 'Hey I  Am Red';
 
 container.appendChild(paragraph);
+
+const heading = document.createElement('h3');
+
+heading.classList.add('heading');
+
+heading.style.color = 'blue';
+
+heading.textContent = 'I am blue h3';
+container.appendChild(heading);
+
+
